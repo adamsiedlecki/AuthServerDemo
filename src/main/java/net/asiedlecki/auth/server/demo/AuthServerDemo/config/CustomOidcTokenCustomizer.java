a338edaeb7a2e3c5de0e398fd24d5d61336202fa
adamsiedlecki <adamsiedlec@gmail.com>
2025-12-18 16:46:55 +0100

    change OAuth2TokenClaimsContext to JwtEncodingContext

--- src/main/java/net/asiedlecki/auth/server/demo/AuthServerDemo/config/CustomOidcTokenCustomizer.java
+++ src/main/java/net/asiedlecki/auth/server/demo/AuthServerDemo/config/CustomOidcTokenCustomizer.java
@@ -1,18 +1,18 @@
 package net.asiedlecki.auth.server.demo.AuthServerDemo.config;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.oauth2.server.authorization.token.OAuth2TokenClaimsContext;
+import org.springframework.security.oauth2.server.authorization.token.JwtEncodingContext;
 import org.springframework.security.oauth2.server.authorization.token.OAuth2TokenCustomizer;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
-public class CustomOidcTokenCustomizer implements OAuth2TokenCustomizer<OAuth2TokenClaimsContext> {
+public class CustomOidcTokenCustomizer implements OAuth2TokenCustomizer<JwtEncodingContext> {
 
     private final UsersConfig usersConfig;
 
     @Override
-    public void customize(OAuth2TokenClaimsContext context) {
+    public void customize(JwtEncodingContext context) {
         var principalName = context.getPrincipal().getName();
         var testUser = usersConfig.getUsers().stream()
                 .filter(u -> u.getUsername().equals(principalName))
